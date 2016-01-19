a4d19ae51dc2b734abf066228d546d0353054a82
johnshawstudent <john.shaw@mygeorgian.ca>
2016-01-19 14:03:40 -0500

    rock paper scissors lab

---
+++ rockpaperscissors.js
@@ -0,0 +1,45 @@
+var choice1 = prompt("Do you choose rock, paper or scissors?");
+var choice2 = Math.random();
+if (choice2 < 0.34) {
+    choice2 = "rock";
+} else if(choice2 <= 0.67) {
+    choice2 = "paper";
+} else {
+    choice2 = "scissors";
+} console.log("Computer: " + choice2);
+
+var compare = function (choice1, choice2) {
+    if (choice1 === choice2) {
+        return ("The result is a tie!");}
+        if(choice1 === choice2) {
+    return "The result is a tie";
+}
+
+else if(choice1 === "rock") {
+
+    if(choice2 === "scissors") {
+        return "rock wins";
+    }
+    else {
+        return "paper wins";
+    }
+
+}
+
+    else if (choice1 === "paper") {
+        if(choice2 === "rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+    else if(choice1 === "scissors") {
+        if (choice2 === "rock") {
+            return "rock wins";
+        }
+        else {
+            return "scissors wins";}
+    }
+};
+compare(choice1, choice2);
